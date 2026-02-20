4cbe2300e2102d513cbe9485af78bbdc4cc584b0
UginRundkvist <103577897+UginRundkvist@users.noreply.github.com>
2026-02-20 18:13:05 +0300

    testserver

--- cmd/testserver/main.go
+++ cmd/testserver/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	// тут будем регистрировать хендлеры
+	mux.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
+	mux.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal error"))
+	})
+
+	mux.HandleFunc("/slow", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(3 * time.Second)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Slow response"))
+	})
 	log.Println("test server started on :8081")
 
 	err := http.ListenAndServe(":8081", mux)
